0a28220646257a1d3d8e7ce47c4f3899c34c1ba4
Richard Young <richard@weaselware.biz>
2019-05-07 16:59:48 -0700

    Incorporated framework of state machine

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,18 @@
 #include <Ethernet.h>
 #include <PubSubClient.h>
 
+enum State_enum {STOP, FORWARD, ROTATE_RIGHT, ROTATE_LEFT};
+enum Sensors_enum {NONE, SENSOR_RIGHT, SENSOR_LEFT, BOTH};
+ 
+void state_machine_run(uint8_t sensors);
+void motors_stop();
+void motors_forward();
+void motors_right();
+void motors_left();
+uint8_t read_IR();
+ 
+uint8_t state = STOP;
+
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network.
 // gateway and subnet are optional:
@@ -15,7 +27,7 @@ IPAddress gateway(192, 168, 1, 199);
 IPAddress subnet(255, 255, 255, 0);
 
 
-IPAddress server(192, 168, 10, 67);
+IPAddress server(192, 168, 10, 22);
 EthernetClient ethclient;
 PubSubClient client(ethclient);
 
@@ -49,13 +61,81 @@ void reconnect() {
 
 }
 
+void state_machine_run(uint8_t sensors) 
+{
+  switch(state)
+  {
+    case STOP:
+      if(sensors == NONE){
+        motors_forward();
+        state = FORWARD;
+      }
+      else if(sensors == SENSOR_RIGHT){
+        motors_left();
+        state = ROTATE_LEFT;
+      }
+      else{
+        motors_right();
+        state = ROTATE_RIGHT;
+      }
+      break;
+       
+    case FORWARD:
+      if(sensors != NONE){
+        motors_stop();
+        state = STOP;
+      }
+      break;
+ 
+    case ROTATE_RIGHT:
+      if(sensors == NONE || sensors == SENSOR_RIGHT){
+        motors_stop();
+        state = STOP;
+      }
+      break;
+ 
+    case ROTATE_LEFT:
+      if(sensors != SENSOR_RIGHT)
+      {
+        motors_stop();
+        state = STOP; 
+      }
+      break;
+  }
+}
+
+void motors_stop()
+{
+  //code for stopping motors
+}
+ 
+void motors_forward()
+{
+  //code for driving forward  
+}
+ 
+void motors_right()
+{
+  //code for turning right
+}
+ 
+void motors_left()
+{
+  //code for turning left
+}
+ 
+uint8_t read_IR()
+{
+  return 1;
+}
+
 void callback(char* topic, byte* payload, unsigned int length) {
 
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
 
-  for (int i=0;i<length;i++) {
+  for (unsigned int i=0;i<length;i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
@@ -79,4 +159,5 @@ void loop() {
     reconnect();
   }
   client.loop();
+  state_machine_run(read_IR());
 }
